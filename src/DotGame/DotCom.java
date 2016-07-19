dad61d5048e1ddc33cc552963eb8543b80c7f877
samno1 <samno1@yandex.ru>
2016-07-20 01:09:13 +0300

    GameDotCom

--- src/DotGame/DotCom.java
+++ src/DotGame/DotCom.java
@@ -3,10 +3,30 @@ package DotGame;
 /**
  * Created by sam on 19.07.16.
  */
-import java.util.ArrayList;
+import java.util.*;
 public class DotCom {
     private  ArrayList<String> locationCells;
+    private  String name;
+    public void setLocationCells(ArrayList<String> locationCells){
+        this.locationCells=locationCells;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
 
-
+    public String CheckYourself(String userInput){
+        String result="Мимо";
+        int index = locationCells.indexOf(userInput);
+        if (index>=0){
+            locationCells.remove(index);
+            if (locationCells.isEmpty()){
+                result="Потопил";
+                System.out.println("Ой! Вы потопили "+name+" : (");
+            }else {
+                result= "Попал";
+            }
+        }
+        return result;
+    }
 
 }
--- src/DotGame/DotComBust.java
+++ src/DotGame/DotComBust.java
@@ -3,7 +3,44 @@ package DotGame;
 /**
  * Created by sam on 19.07.16.
  */
+import java.util.*;
 
 public class DotComBust {
+    private GameHelper helper=new GameHelper();
+    private ArrayList<DotCom> dotComArrayList = new ArrayList<DotCom>();
+    private int numOfGuess=0;
 
+    private void setUpGame(){
+        //Создодим несколько Сайтов и Присвоем им адреса
+        DotCom one = new DotCom();
+        one.setName(" Basket.com");
+        DotCom two = new DotCom();
+        two.setName("Volley.com");
+        DotCom three = new DotCom();
+        three.setName("Foot.com");
+        dotComArrayList.add(one);
+        dotComArrayList.add(two);
+        dotComArrayList.add(three);
+
+        System.out.println("Добро пожаловать на нашу Игру !!");
+        System.out.println("Ваша цель - потопить три сайта");
+        System.out.println("Basket.com, Volley.com, Foot.com");
+        System.out.println("Попытайтесь потопить их за минимальное количество ходов");
+
+        for (DotCom dotComToSet: dotComArrayList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        }
+
+    }
+    private void startPlaying(){
+        while (!dotComArrayList.isEmpty()){
+            String userGuess = helper.getUserInput("Сделайте ход: ");
+            CheckUserGuess(userGuess);
+        }
+        finishGame();
+    }
+    private void CheckUserGuess(String userGuess){
+
+    }
 }
